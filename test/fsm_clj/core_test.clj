ba650a4ddac4c9ba159438b3fd5e054d5810aa7a
fbeline <fe.belineb@gmail.com>
2018-09-16 19:25:02 -0300

    Unit tests

--- test/fsm_clj/core_test.clj
+++ test/fsm_clj/core_test.clj
@@ -1,7 +1,48 @@
 (ns fsm-clj.core-test
   (:require [clojure.test :refer :all]
-            [fsm-clj.core :refer :all]))
+            [fsm-clj.core :as fsm]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn inc-handler [acc message]
+  (or message (inc acc)))
+
+(fsm/defsm traffic-light
+ [[:green -> :yellow when :to-yellow handler `inc-handler]
+  [:yellow -> :red when :to-red handler `inc-handler]
+  [:red -> :green when :to-green handler `inc-handler]
+  [:red -> :yellow when :to-yellow handler `inc-handler]])
+
+(def traffic-light-fsm (traffic-light 0))
+
+(deftest fsm-test
+  (testing "By default first transition state is the start state"
+    (is (-> traffic-light-fsm :state (= :green))))
+
+  (testing "We can set an accumulator at fsm creation"
+    (let [fsm (traffic-light 10)]
+      (is (-> fsm :acc (= 10)))))
+
+  (testing "We can set initial state at fsm creation"
+    (let [fsm (traffic-light 0 :red)]
+      (is (-> fsm :state (= :red)))))
+
+  (testing "A valid transition should change the state"
+    (is (-> traffic-light-fsm (fsm/send-event :to-yellow) :state (= :yellow))))
+
+  (testing "An invalid transition should not change the state"
+    (is (-> traffic-light-fsm (fsm/send-event :to-red) :state (= :green))))
+
+  (testing "The transition handler should be executed"
+    (is (-> traffic-light-fsm
+            (fsm/send-event :to-yellow)
+            (fsm/send-event :to-red)
+            :acc
+            (= 2))))
+
+  (testing "We can pass message to transition handler"
+    (is (-> traffic-light-fsm
+            (fsm/send-event :to-yellow)
+            (fsm/send-event :to-red -1)
+            :acc
+            (= -1))))
+
+  )
