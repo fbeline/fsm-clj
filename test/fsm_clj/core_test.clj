816fdd9017daaddfb3d8884d4bd1388995da5e5f
fbeline <fe.belineb@gmail.com>
2018-09-18 20:05:07 -0300

    renaming tests descriptions

--- test/fsm_clj/core_test.clj
+++ test/fsm_clj/core_test.clj
@@ -30,21 +30,21 @@
   (testing "An invalid transition should not change the state"
     (is (-> traffic-light-fsm (fsm/send-event :to-red) :state (= :green))))
 
-  (testing "The transition handler should be executed"
+  (testing "The transition action should be executed"
     (is (-> traffic-light-fsm
             (fsm/send-event :to-yellow)
             (fsm/send-event :to-red)
             :acc
             (= 2))))
 
-  (testing "We can pass message to transition handler"
+  (testing "We can pass message to transition in send-event"
     (is (-> traffic-light-fsm
             (fsm/send-event :to-yellow)
             (fsm/send-event :to-red -1)
             :acc
             (= -1))))
 
-  (testing "No action defined"
+  (testing "Is ok to have no action defined"
     (is (-> (traffic-light 0 :red)
             (fsm/send-event :to-green)
             :acc
