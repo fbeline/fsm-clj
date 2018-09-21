02cbc4dc3a5ad7b8911a58e0135cefaa1ef57aa2
fbeline <fe.belineb@gmail.com>
2018-09-21 01:42:40 -0300

    Project version update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fsm-clj "0.1.0"
+(defproject fsm-clj "0.1.1"
   :description "A Clojure library designed to create deterministic finite state machines."
   :url "https://github.com/fbeline/fsm-clj"
   :license {:name "Eclipse Public License"
